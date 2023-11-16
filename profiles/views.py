e701af0d6a523a1c03c52f076127b56fea4e28fe
Karlox01 <orlandinikarlo@gmail.com>
2023-11-16 20:56:52 +0000

    added postlist view tests

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
-from . serializers import ProfileSerializer
+from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
 
