95ce499eb7367f3467bac7bafcd53cf281921721
Karlox01 <orlandinikarlo@gmail.com>
2023-11-16 21:21:03 +0000

    added post detail tests

--- posts/tests.py
+++ posts/tests.py
@@ -30,3 +30,42 @@ class PostListViewTests(APITestCase):
         print(response.data)
         print(len(response.data))
 
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        adam = User.objects.create_user(username='adam', password='password')
+        brian = User.objects.create_user(username='brian', password='password')
+        Post.objects.create(
+            owner=adam, title='a title 1', content='my content'
+        )
+        Post.objects.create(
+            owner=brian, title='a title 2', content='my content two' 
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/2/')
+        self.assertEqual(response.data['title'], 'a title 2')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='adam', password='password')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+        
+
+    def test_user_can_update_other_posts(self):
+        self.client.login(username='brian', password='password')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+        
+
+
+
+
